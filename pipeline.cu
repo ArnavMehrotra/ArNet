8fa45af6a6dbf4c68b01628803c00620b6a938b4
ArnavMehrotra <arnavm22@yahoo.com>
2025-08-04 18:39:48 -1000

    remove some ops for debugging

--- pipeline.cu
+++ pipeline.cu
@@ -22,33 +22,40 @@ extern "C" void forward_pass(float* X, float* W1, float* B1, float* W2, float* B
 
     Tensor<float>* t_h = new Tensor<float>({J, M});
     Tensor<float>* t_z = new Tensor<float>({J, M});
-    Tensor<float>* t_z_relu = new Tensor<float>({J, M});
+    // Tensor<float>* t_z_relu = new Tensor<float>({J, M});
 
-    Tensor<float>* t_w2 = new Tensor<float>(W2, {M, N});
-    Tensor<float>* t_b2 = new Tensor<float>(B2, {N});
-    Tensor<float>* t_r = new Tensor<float>({J, N});
-    Tensor<float>* t_y = new Tensor<float>({J, N});
-    Tensor<float>* t_y_softmax = new Tensor<float>({J, N});
+    // Tensor<float>* t_w2 = new Tensor<float>(W2, {M, N});
+    // Tensor<float>* t_b2 = new Tensor<float>(B2, {N});
+    // Tensor<float>* t_r = new Tensor<float>({J, N});
+    // Tensor<float>* t_y = new Tensor<float>({J, N});
+    // Tensor<float>* t_y_softmax = new Tensor<float>({J, N});
     
     std::vector<Op<float>*> ops = {
         new Gemm<float>({t_x, t_w1, t_h}),
         new BiasAdd<float>({t_h, t_b1, t_z}),
-        new Relu<float>({t_z, t_z_relu}),
-        new Gemm<float>({t_z_relu, t_w2, t_r}),
-        new BiasAdd<float>({t_r, t_b2, t_y}),
-        new Softmax<float> ({t_y, t_y_softmax}),
+        // new Relu<float>({t_z, t_z_relu}),
+        // new Gemm<float>({t_z_relu, t_w2, t_r}),
+        // new BiasAdd<float>({t_r, t_b2, t_y}),
+        // new Softmax<float> ({t_y, t_y_softmax}),
     };
 
     Net nn = Net(ops);
     nn.forward();
 
-    float* result = t_y_softmax->to_host();
-    memcpy(out, result, J * N * sizeof(float));
+    delete t_x; delete t_w1; delete t_b1; delete t_h; delete t_z;
+
+    float *result = t_h->to_host();
+    memcpy(out, result, J * M * sizeof(float));
     free(result);
 
-    delete t_x; delete t_w1; delete t_b1; delete t_w2; delete t_b2;
-    delete t_h; delete t_z_relu; delete t_y; delete t_y_softmax;
-    delete t_z; delete t_z_relu; delete t_r;
+
+    // float* result = t_y_softmax->to_host();
+    // memcpy(out, result, J * N * sizeof(float));
+    // free(result);
+
+    // delete t_x; delete t_w1; delete t_b1; delete t_w2; delete t_b2;
+    // delete t_h; delete t_z_relu; delete t_y; delete t_y_softmax;
+    // delete t_z; delete t_z_relu; delete t_r;
 
     for (Op<float>* op : ops) {
         delete op;
