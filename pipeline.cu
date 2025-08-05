f392743d84574009fd07d01f7f4c695be2c8f215
ArnavMehrotra <arnavm22@yahoo.com>
2025-08-04 19:02:25 -1000

    add softmax to pipeline

--- pipeline.cu
+++ pipeline.cu
@@ -28,7 +28,7 @@ extern "C" void forward_pass(float* X, float* W1, float* B1, float* W2, float* B
     Tensor<float>* t_b2 = new Tensor<float>(B2, {N});
     Tensor<float>* t_r = new Tensor<float>({J, N});
     Tensor<float>* t_y = new Tensor<float>({J, N});
-    // Tensor<float>* t_y_softmax = new Tensor<float>({J, N});
+    Tensor<float>* t_y_softmax = new Tensor<float>({J, N});
     
     std::vector<Op<float>*> ops = {
         new Gemm<float>({t_x, t_w1, t_h}),
@@ -36,28 +36,19 @@ extern "C" void forward_pass(float* X, float* W1, float* B1, float* W2, float* B
         new Relu<float>({t_z, t_z_relu}),
         new Gemm<float>({t_z_relu, t_w2, t_r}),
         new BiasAdd<float>({t_r, t_b2, t_y}),
-        // new Softmax<float> ({t_y, t_y_softmax}),
+        new Softmax<float> ({t_y, t_y_softmax}),
     };
 
     Net nn = Net(ops);
     nn.forward();
 
-    float *result = t_y->to_host();
+    float *result = t_y_softmax->to_host();
     memcpy(out, result, J * N * sizeof(float));
     free(result);
 
     delete t_x; delete t_w1; delete t_b1; delete t_h; delete t_z;
     delete t_z_relu; delete t_w2; delete t_r; delete t_b2; delete t_y;
-
-
-
-    // float* result = t_y_softmax->to_host();
-    // memcpy(out, result, J * N * sizeof(float));
-    // free(result);
-
-    // delete t_x; delete t_w1; delete t_b1; delete t_w2; delete t_b2;
-    // delete t_h; delete t_z_relu; delete t_y; delete t_y_softmax;
-    // delete t_z; delete t_z_relu; delete t_r;
+    delete t_y_softmax;
 
     for (Op<float>* op : ops) {
         delete op;
