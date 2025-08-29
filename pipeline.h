d5da973b4ef0afea84d7e95931bc37d38a1fe9cb
ArnavMehrotra <arnavm22@yahoo.com>
2025-08-28 15:05:33 -1000

    added old forward method back for old test

--- pipeline.h
+++ pipeline.h
@@ -92,6 +92,12 @@ class Net {
             }
         }
 
+        void forward() {
+            for (Op<T>* op : _ops) {
+                op->forward();
+            }
+        }
+
         void backward() {
             for (auto it = _ops.rbegin(); it != _ops.rend(); ++it) {
                 (*it)->backward();
