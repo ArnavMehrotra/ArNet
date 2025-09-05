7f9bf684eccceee7f83d3b9f6f6e00f8b45c675d
ArnavMehrotra <arnavm22@yahoo.com>
2025-09-05 12:05:51 -1000

    may still be double free in softmax

--- pipeline.h
+++ pipeline.h
@@ -63,7 +63,6 @@ class Net {
             //softmax for logits
             Tensor<T> *y = _ops.back()->tensors().back();
             Tensor<T> *logits = make_tensor({n, out_dim});
-            return;
             _ops.push_back(new Softmax<T>({y, logits}));
         }
 
